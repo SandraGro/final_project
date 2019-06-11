3c948b1da130539bb4dd5362dc60ffde806706ab
SandraGro <sandra.guerreroflores@gmail.com>
2019-06-11 12:44:16 -0500

    se agregan rutas y componete home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SelectorHome  from './SelectorHome/SelectorHome';
+import Slider from './Slider/Slider';
+
+const Home = () => (
+    <>
+    <Slider/>
+    <SelectorHome/>
+    </>
+);
+
+export default Home;
---
+++ src/components/Home/SelectorHome/SelectorHome.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Card, CardDeck } from 'react-bootstrap';
+import './SelectorHome.css';
+
+const SelectorHome = () => (
+  <div className="padd">
+    <p className="title">¿Qué estás buscando?</p>
+    <CardDeck>
+      <Card>
+        <Card.Img variant="top" src="/assets/breakfast2.jpg" />
+        <Card.Footer>
+          <Card.Title className="category">Breakfast</Card.Title>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="/assets/lunch1.jpg" />
+        <Card.Footer>
+          <Card.Title className="category">Lunch</Card.Title>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="/assets/dinner1.jpeg" />
+        <Card.Footer>
+          <Card.Title className="category">Dinner</Card.Title>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="/assets/dessert3.jpg" />
+        <Card.Footer>
+          <Card.Title className="category">Dessert</Card.Title>
+        </Card.Footer>
+      </Card>
+    </CardDeck>
+  </div>
+);
+
+export default SelectorHome;

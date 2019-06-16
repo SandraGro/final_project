8eb0301c3469a53427f4bcbe5525f17322a46be9
Sandra Alejandra <sandra.guerreroflores@gmail.com>
2019-06-15 20:30:40 -0500

    Consume categorías del backend

--- src/components/Home/SelectorHome/SelectorHome.jsx
+++ src/components/Home/SelectorHome/SelectorHome.jsx
@@ -1,40 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card, CardDeck } from 'react-bootstrap';
 import Link from 'react-router-dom/Link';
+import { getData } from '../../../utils/api';
 import './SelectorHome.css';
 
-const SelectorHome = () => (
-  <div className="padd">
-    <p className="title">¿Qué estás buscando?</p>
-    <CardDeck>
-      <Card>
-      <Link className="links" to ="/breakfast" >
-<Card.Img variant="top" src="/assets/breakfast2.jpg" />
-        <Card.Footer>
-          <Card.Title className="category">Breakfast</Card.Title>
-        </Card.Footer>
-      </Link>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="/assets/lunch1.jpg" />
-        <Card.Footer>
-          <Card.Title className="category">Lunch</Card.Title>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="/assets/dinner1.jpeg" />
-        <Card.Footer>
-          <Card.Title className="category">Dinner</Card.Title>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="/assets/dessert3.jpg" />
-        <Card.Footer>
-          <Card.Title className="category">Dessert</Card.Title>
-        </Card.Footer>
-      </Card>
-    </CardDeck>
-  </div>
-);
+class SelectorHome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: []
+    }
+    this.getCategories = this.getCategories.bind(this);
+  }
+
+  getCategories() {
+    getData("categories").then(categories => this.setState({ categories }))
+  }
+
+  componentDidMount() {
+    this.getCategories();
+  }
+
+  render() {
+    return (
+      <>
+        <div className="padd">
+          <p className="title">¿Qué estás buscando?</p>
+          <CardDeck>
+            {
+              this.state.categories.map((category) => {
+                return <Card>
+                  <Link className="links" to={category['path']} >
+                    <Card.Img variant="top" src={category['image']} />
+                    <Card.Footer>
+                      <Card.Title className="category">{category['title']}</Card.Title>
+                    </Card.Footer>
+                  </Link>
+                </Card>
+              })
+            }
+          </CardDeck>
+        </div>
+      </>
+    );
+  }
+}
 
 export default SelectorHome;
